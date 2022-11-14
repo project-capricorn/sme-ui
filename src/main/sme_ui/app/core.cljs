6a868969837eb372d32875057c2c1006831d76a8
Andrew Brown <a.c.brown@protonmail.com>
2022-11-14 17:52:53 -0600

    Expand key-pad and add expression field

--- src/main/sme_ui/app/core.cljs
+++ src/main/sme_ui/app/core.cljs
@@ -2,21 +2,35 @@
   (:require [reagent.dom :as rdom])
   (:require [reagent.core :as r]))
 
-(defn keypad [] 
-  [:div 
+(def a-set (r/atom \S))
+
+(def an-expression (r/atom "relation"))
+
+(defn set-builder [] [:div 
+                      [:h3 "{ x \u2208 "  
+                       [:span {:style {:border-style "dashed" :border-color "red" :padding-left "4px" :padding-right "4px"}} @a-set] " | " 
+                       [:span {:style {:border-style "dashed" :border-color "red" :padding-left "4px" :padding-right "4px"}} @an-expression] " }"]])
+
+(defn keypad []
+  [:div
    [:h3 "Common sets"]
-   [:button {:title "Negative real numbers" :style {:margin "5px"}} "\u211D-"]
-   [:button {:title "Real numbers" :style {:margin "5px"}} \u211D]
-   [:button {:title "Positive real numbers" :style {:margin "5px"}} "\u211D+"]
+   [:button {:title "Negative real numbers" :style {:margin "5px"} :on-click #(reset! a-set "\u211D-")} "\u211D-"]
+   [:button {:title "Real numbers" :style {:margin "5px"} :on-click #(reset! a-set "\u211D")} \u211D]
+   [:button {:title "Positive real numbers" :style {:margin "5px"} :on-click #(reset! a-set "\u211D+")} "\u211D+"]
+   [:button {:title "Negative integers" :style {:margin "5px"} :on-click #(reset! a-set "\u2124-")} "\u2124-"]
+   [:button {:title "Integers" :style {:margin "5px"} :on-click #(reset! a-set "\u2124")} \u2124]
+   [:button {:title "Positive integers" :style {:margin "5px"} :on-click #(reset! a-set "\u2124+")} "\u2124+"]
+   [:button {:title "Natural numbers" :style {:margin "5px"} :on-click #(reset! a-set "\u2115")} "\u2115"]
    [:h3 "Relational operators"]
    [:button {:title "Less than" :style {:margin "5px"}} \<]
    [:button {:title "Less than or equal to" :style {:margin "5px"}} \u2264]
-   [:button {:title "Equals" :style {:margin "5px"}} \=]
+   [:button {:title "Equal to" :style {:margin "5px"}} \=]
+   [:button {:title "Not equal to" :style {:margin "5px"}} \u2260]
    [:button {:title "Greater than or equal to" :style {:margin "5px"}} \u2265]
    [:button {:title "Great than" :style {:margin "5px"}} \>]])
 
 (defn render []
-  (rdom/render [:div [keypad]] (.getElementById js/document "root")))
+  (rdom/render [:div [keypad] [set-builder]] (.getElementById js/document "root")))
 
 (defn ^:export main []
   (render))

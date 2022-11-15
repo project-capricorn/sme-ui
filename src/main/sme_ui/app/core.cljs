1ecd95a0b01114a9bc50a13ee8146bc90a5625c5
Andrew Brown <a.c.brown@protonmail.com>
2022-11-15 17:38:29 -0600

    Begin adding predicate behavior with CSS shorthand

--- src/main/sme_ui/app/core.cljs
+++ src/main/sme_ui/app/core.cljs
@@ -2,14 +2,18 @@
   (:require [reagent.dom :as rdom])
   (:require [reagent.core :as r]))
 
-(def a-set (r/atom \S))
+(def predicate-placeholder ["P(x)"])
 
-(def an-expression (r/atom "relation"))
+(def set-placeholder "Set")
+
+(def a-set (r/atom set-placeholder))
+
+(def predicate (r/atom predicate-placeholder))
 
 (defn set-builder [] [:div 
                       [:h3 "{ x \u2208 "  
-                       [:span {:style {:border-style "dashed" :border-color "red" :padding-left "4px" :padding-right "4px"}} @a-set] " | " 
-                       [:span {:style {:border-style "dashed" :border-color "red" :padding-left "4px" :padding-right "4px"}} @an-expression] " }"]])
+                       [:span.border-red @a-set] " | " 
+                       [:span.border-red (apply str @predicate)] " }"]])
 
 (defn keypad []
   [:div
@@ -22,12 +26,21 @@
    [:button {:title "Positive integers" :style {:margin "5px"} :on-click #(reset! a-set "\u2124+")} "\u2124+"]
    [:button {:title "Natural numbers" :style {:margin "5px"} :on-click #(reset! a-set "\u2115")} "\u2115"]
    [:h3 "Relational operators"]
-   [:button {:title "Less than" :style {:margin "5px"}} \<]
-   [:button {:title "Less than or equal to" :style {:margin "5px"}} \u2264]
+   [:button {:title "Less than" :style {:margin "5px"} 
+             :on-click #(swap! predicate conj \<)} \<]
+   [:button {:title "Less than or equal to" :style {:margin "5px"}
+             :on-click #(swap! predicate conj \u2264)} \u2264]
    [:button {:title "Equal to" :style {:margin "5px"}} \=]
    [:button {:title "Not equal to" :style {:margin "5px"}} \u2260]
    [:button {:title "Greater than or equal to" :style {:margin "5px"}} \u2265]
-   [:button {:title "Great than" :style {:margin "5px"}} \>]])
+   [:button {:title "Great than" :style {:margin "5px"}} \>]
+   [:h3 "Predicate Controls"]
+   [:button {:title "Reset the predicate" 
+             :style {:margin "5px"} 
+             :on-click #(
+                        (reset! a-set set-placeholder) 
+                        (reset! predicate predicate-placeholder))} "Reset"]
+   [:button {:title "Delete last charater" :style {:margin "5px"}} "Backspace"]])
 
 (defn render []
   (rdom/render [:div [keypad] [set-builder]] (.getElementById js/document "root")))

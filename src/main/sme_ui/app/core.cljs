e13e7f4b95c01a554f5b08d64242c538d6df8c43
Andrew Brown <a.c.brown@protonmail.com>
2022-11-16 10:50:48 -0600

    Work on building buttons dynamically

--- src/main/sme_ui/app/core.cljs
+++ src/main/sme_ui/app/core.cljs
@@ -10,33 +10,38 @@
 
 (def predicate (r/atom predicate-placeholder))
 
-(defn set-builder [] [:div 
-                      [:h3 "{ x \u2208 "  
-                       [:span.border-red @a-set] " | " 
+(def set-symbols (sorted-map "Real numbers" \u211D
+                             "Integers" \u2124
+                             "Natural numbers" \u2115))
+
+(defn set-buttons [] (reduce
+                      (fn [acc cur] (conj acc
+                                          [:button.pad-keys {:title (key cur)
+                                                             :on-click #(reset! a-set (val cur))}
+                                           (val cur)])) [] set-symbols))
+
+(defn set-builder [] [:div
+                      [:h3 "{ x \u2208 "
+                       [:span.border-red @a-set] " | "
                        [:span.border-red (apply str @predicate)] " }"]])
 
 (defn keypad []
   [:div
    [:h3 "Common sets"]
-   [:button.pad-keys {:title "Negative real numbers" :on-click #(reset! a-set "\u211D-")} "\u211D-"]
-   [:button.pad-keys {:title "Real numbers" :on-click #(reset! a-set "\u211D")} \u211D]
-   [:button.pad-keys {:title "Positive real numbers" :on-click #(reset! a-set "\u211D+")} "\u211D+"]
-   [:button.pad-keys {:title "Negative integers" :on-click #(reset! a-set "\u2124-")} "\u2124-"]
-   [:button {:title "Integers"  :on-click #(reset! a-set "\u2124")} \u2124]
-   [:button.pad-keys {:title "Positive integers" :on-click #(reset! a-set "\u2124+")} "\u2124+"]
-   [:button.pad-keys {:title "Natural numbers" :on-click #(reset! a-set "\u2115")} "\u2115"]
+   [:button.pad-keys {:title "Real numbers" :on-click #(reset! a-set \u211D)} \u211D]
+   [:button {:title "Integers"  :on-click #(reset! a-set \u2124)} \u2124]
+   [:button.pad-keys {:title "Natural numbers" :on-click #(reset! a-set \u2115)} \u2115]
    [:h3 "Relational operators"]
    [:button.pad-keys {:title "Less than" :on-click #(swap! predicate conj \<)} \<]
    [:button.pad-keys {:title "Less than or equal to" :on-click #(swap! predicate conj \u2264)} \u2264]
-   [:button.pad-keys {:title "Equal to" } \=]
-   [:button.pad-keys {:title "Not equal to" } \u2260]
-   [:button.pad-keys {:title "Greater than or equal to" } \u2265]
-   [:button.pad-keys {:title "Great than" } \>]
+   [:button.pad-keys {:title "Equal to"} \=]
+   [:button.pad-keys {:title "Not equal to"} \u2260]
+   [:button.pad-keys {:title "Greater than or equal to"} \u2265]
+   [:button.pad-keys {:title "Great than"} \>]
    [:h3 "Predicate Controls"]
    [:button.pad-keys {:title "Reset the predicate"
-             :on-click #(
-                        (reset! a-set set-placeholder) 
-                        (reset! predicate predicate-placeholder))} "Reset"]
+                      :on-click #((reset! a-set set-placeholder)
+                                  (reset! predicate predicate-placeholder))} "Reset"]
    [:button.pad-keys {:title "Delete last charater"} "Backspace"]])
 
 (defn render []

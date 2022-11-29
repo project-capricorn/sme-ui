078f00cbbab3caae15cb08c3586c7a2cbd3d60f3
Andrew Brown <a.c.brown@protonmail.com>
2022-11-28 18:59:19 -0600

    Work on SME Set Builder description.

--- src/main/sme_ui/app/core.cljs
+++ src/main/sme_ui/app/core.cljs
@@ -17,23 +17,19 @@
 
 (def log-pred (log/cons-log-atom predicate))
 
-(defn buttons-from [mappings f] (reduce
-                                 (fn [acc cur]
-                                   (let [sym (key cur) desc (val cur)]
-                                     (conj acc [:button.pad-keys
-                                                {:title desc :on-click #(f sym)}
-                                                sym]))) [:div] mappings))
+(defn buttons-from [mappings f] (reduce (fn [acc cur]
+                                          (let [sym (key cur) desc (val cur)]
+                                            (conj acc [:button.pad-keys
+                                                       {:title desc :on-click #(f sym)}
+                                                       sym]))) [:div] mappings))
 
-(defn validate-pred [if-case else-case & fns] (if (every? true? (map #(% @predicate) fns)) 
-                                                if-case else-case))
+(defn validate-pred [if-case else-case & fns] (if (every? true? (map #(% @predicate) fns)) if-case else-case))
 
 (def border-pred (partial validate-pred :span.border-green :span.border-red))
 
 (def disable-pred-eval (partial validate-pred false true))
 
-(defn validate-set-sym [if-case else-case] (if (some #(= @a-set %) (keys sym/set-sym))
-                            if-case
-                            else-case))
+(defn validate-set-sym [if-case else-case] (if (some #(= @a-set %) (keys sym/set-sym)) if-case  else-case))
 
 (def border-set-sym (partial validate-set-sym :span.border-green :span.border-red))
 
@@ -43,34 +39,30 @@
 
 (defn disable-eval [] (or (disable-set-eval) (apply disable-pred-eval validators)))
 
-(defn set-builder [] [:div
-                      [:h3 "{ x \u2208 "
-                       [(border-set-sym) {:title "Set"} @a-set] " | "
-                       [(apply border-pred validators) {:title "Predicate"}
-                        (string/join " " @predicate)] " }"]])
 
-(defn clear-pred-place! [] (when (= (first @predicate)
-                                   (first predicate-placeholder))
-                            (reset! predicate [])))
+(defn clear-pred-place! [] (when (= (first @predicate) (first predicate-placeholder))
+                             (reset! predicate [])))
 
 (defn append-to-pred! [x] (clear-pred-place!) (swap! predicate conj x))
 
+(defn concat-num! [x] (let [num (last @predicate)] (if (and (not= num \u002D) (util/non-numeric? num))
+                                                     (append-to-pred! x)
+                                                     (do
+                                                       (clear-pred-place!)
+                                                       (swap! predicate pop)
+                                                       (swap! predicate conj (str num x))))))
 
-(defn concat-num! [x] (let [num (last @predicate)]
-                        (if 
-                            (and 
-                             (not= num \u002D) 
-                             (util/non-numeric? num))
-                         (append-to-pred! x)
-                         (do
-                           (clear-pred-place!)
-                           (swap! predicate pop)
-                           (swap! predicate conj (str num x))))))
+(defn header [] [:div [:header[:h1 "SME Online"]]])
+
+(defn notes [] [:div [:p "The SME Set Builder currently allows you to build sets by applying syntactically meaningful predicates to integers."]
+                [:p "The set builder notation {x \u2208 S | P(x)} is read formally as 'The set of all elements x in S such that P(x) is true' 
+where \u2208 denotes 'is an element of', S represents a set, x an element of that set, and P(x) is a function that evaluates to true or false (a predicate). Finally, | means 'such that', indicating the predicate should evaluate to true given x."]
+                [:p "A syntactically meaningful predicate is one that can be evaluated (computed), although it may be obviously true or false. 
+For instance, it is meaningful to apply the predicate 6 < 3 to the set of natural numbers insofar as the predicate is always false.
+It is not meaningful to apply 6 < because the expression is incomplete."]])
 
 (defn keypad []
   [:div
-   [:header
-    [:h1 "SME Online"]]
    [:h3 "Sets"]
    (buttons-from sym/set-sym (fn [val] (reset! a-set val)))
    [:h3 "Binary Operators"]
@@ -85,13 +77,19 @@
    [:button.pad-keys {:title "Reset the predicate"
                       :on-click #((reset! a-set set-placeholder)
                                   (reset! predicate predicate-placeholder))} "Reset"]
-   [:button.pad-keys {:title "Delete last charater" 
+   [:button.pad-keys {:title "Delete last charater"
                       :on-click #(swap! predicate pop)} "Backspace"]
-   [:button.pad-keys {:title "Evaluate expression" 
+   [:button.pad-keys {:title "Evaluate expression"
                       :disabled (disable-eval)} "Eval"]])
 
+(defn set-builder [] [:div
+                      [:h3 "{ x \u2208 "
+                       [(border-set-sym) {:title "Set"} @a-set] " | "
+                       [(apply border-pred validators) {:title "Predicate"}
+                        (string/join " " @predicate)] " }"]])
+
 (defn render []
-  (rdom/render [:div [keypad] [set-builder]] (.getElementById js/document "root")))
+  (rdom/render [:div [header] [notes] [keypad] [set-builder]] (.getElementById js/document "root")))
 
 (defn ^:export main []
   (render))

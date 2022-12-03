b004412468870d1294ee05389585fc6aa75a6a78
Andrew Brown <a.c.brown@protonmail.com>
2022-12-03 10:11:15 -0600

    Update deploy script to include index.html

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,7 @@
 npm run build
 cp public/css/style.css       ../project-capricorn.github.io/css/
 cp public/js/main.js ../project-capricorn.github.io/js/
+cp public/index.html ../project-capricorn.github.io/
 cd ../project-capricorn.github.io
 git add .
 git commit -m "Automated deployment commit"

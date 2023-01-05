2a617b84172245504ac785b66ace1f89845c3874
Andrew Brown <a.c.brown@protonmail.com>
2023-01-05 16:03:21 -0600

    Additional geo functions

--- src/main/sme_ui/app/geo.cljs
+++ src/main/sme_ui/app/geo.cljs
@@ -1,15 +1,70 @@
 (ns sme-ui.app.geo
   (:require  [sme-ui.app.util :as util]))
 
-(defn to-radians [degrees]
-  "Convert from degrees to radians"
+(defn to-radians
+  "Converts from degrees to radians"
+  [degrees]
   (* degrees (/ Math/PI 180)))
 
-(defn to-cartesian [rho theta]
-  "Convert from polar to cartesian coordinates"
-  (let [x (* rho (Math/cos (to-radians theta)))
-        y (* rho (Math/sin (to-radians theta)))]
+(defn to-degrees
+  "Converts from radians to degrees"
+  [radians]
+  (* radians (/ 180 Math/PI)))
+
+(defn to-cartesian
+  "Converts from polar to cartesian coordinates"
+  [r theta]
+  (let [x (* r (Math/cos (to-radians theta)))
+        y (* r (Math/sin (to-radians theta)))]
     [x y]))
+
+(defn get-angle
+  "Returns an angle in radians"
+  [point]
+  (let [[x y] point
+        angle (Math/atan2 x y)]
+    (if (<= angle 0)
+      (+ (* 2 Math/PI) angle)
+      angle)))
+
+(defn get-distance
+  "Returns the distance of a point from the origin"
+  [point]
+  (let [[x y] point]
+    (Math/sqrt (+ (* x x) (* y y)))))
+
+(defn transfrom
+  "Shift a vector of points by x-shift, y-shift"
+  [points x-shift y-shift]
+  (map (fn [point] (let [[x y] point] [(+ x x-shift) (+ y y-shift)])) points))
+
+(defn get-centroid
+  "Returns the geometric center of points"
+  [points]
+  (let [num-points (count points)
+        x-mean (/ (apply + (map first points)) num-points)
+        y-mean (/ (apply + (map second points)) num-points)]
+    [x-mean y-mean]))
+
+(defn compare-clockwise
+  ""
+  [p1 p2]
+  (let [a1 (get-angle p1)
+        a2 (get-angle p2)
+        d1 (get-distance p1)
+        d2 (get-distance p2)]
+    (cond
+      (> a1 a2) true
+      (and (= a1 a2) (< d1 d2)) true
+      :else false)))
+
+(defn sort-anti-clock [points]
+  (let [n (count points)
+        [x-mean y-mean] (get-centroid n points) 
+        trans (transform points (- x-mean) (- y-mean))]
+    trans))
+
+(sort-anti-clock [[-2 2] [-2 1] [-1 0] [-3 1]])
 
 (defn lace
   "Determines the area of a simple polygon via the Shoelace Formula. 

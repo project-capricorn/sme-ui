c51b237cd0364f4ff9071b2bdf116d4364dbd598
Andrew Brown <a.c.brown@protonmail.com>
2023-01-26 10:59:48 -0600

    Fix copy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,7 @@ npm run build
 cp public/css/style.css       ../project-capricorn.github.io/css/
 cp public/js/main.js ../project-capricorn.github.io/js/
 cp public/index.html ../project-capricorn.github.io/
-cp public/compass.jpeg ../project-capricorn.github.io/
+cp public/compass.jpg ../project-capricorn.github.io/
 cd ../project-capricorn.github.io
 git add .
 git commit -m "Automated deployment commit"

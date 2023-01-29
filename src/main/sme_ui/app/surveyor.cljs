b214f863668d8f7b6d5fbac6545163abe5589301
Andrew Brown <a.c.brown@protonmail.com>
2023-01-29 16:42:37 -0600

    Add on change to inputs

--- src/main/sme_ui/app/surveyor.cljs
+++ src/main/sme_ui/app/surveyor.cljs
@@ -7,26 +7,35 @@
 
 (def polygon (r/atom polygon-placeholder))
 
-(defn input-component [i] [:div
-                           [:div.well
-                            [:label {:for (str "a" i)} (str "Direction " i)]
-                            [:input {:type "number" :id (str "a" i)}]
-                            [:label {:for (str "d" i)} (str "Distance " i)]
-                            [:input {:type "number" :id (str "d" i)}]]])
+(def inputs (r/atom []))
+
+(defn input-component [i]
+  (let [dir (r/atom 0) dis (r/atom 0)]
+    [:div
+     [:div.well
+      [:label {:for (str "dir" i)} (str "Direction " i)]
+      [:input {:type "number"
+               :id (str "dir" i)
+               :on-change #(reset! dir (-> % .-target .-value))}]
+      [:label {:for (str "dis" i)} (str "Distance " i)]
+      [:input {:type "number"
+               :id (str "dis" i)
+               :on-change #(reset! dis (-> % .-target .-value))}]]]))
 
 (def point-num (r/atom 1))
 
-(def inputs (r/atom [:div (input-component @point-num)]))
+(def input-components (r/atom [:div (input-component @point-num)]))
 
 (defn stringify-points [poly]
   (apply str (reduce #(conj %1 (str (string/join "," %2) " ")) [] poly)))
 
+;; TODO :: Extract input values
 (defn parse-inputs [input-components]
   (let [[_ [_ & i]] input-components
         pts (filter #(= :input (first %)) i)]
     (loop [acc [] rem pts]
       (let [[dir dis] rem]
-        (if (nil? dis) acc
+        (if (nil? dir) acc
             (recur (conj acc [(component/parse-int dir)
                               (component/parse-int dis)])
                    (next (rest rem))))))))
@@ -69,21 +78,23 @@
                  [:div.col-sm-3
                   [:h3 {:style {:color "red"}} "In flight"]
                   [:button.btn-primary {:title "Add Point"
-                                        :on-click #(swap! inputs conj
+                                        :on-click #(swap! input-components conj
                                                           (input-component (swap! point-num inc)))}
                    "Add Point"]
                   [:button.btn-warning {:title "Remove Point"
                                         :on-click #(when (> @point-num 1)
                                                      (do (swap! point-num dec)
-                                                         (swap! inputs pop)))}
+                                                         (swap! input-components pop)))}
                    "Remove Point"]
                   [:button.btn-danger {:title "Clear Points"
-                                       :on-click #(do (reset! point-num 0)
-                                                      (reset! inputs [:div]))}
+                                       :on-click #(do
+                                                    (reset! point-num 0)
+                                                    (reset! input-components [:div]))}
                    "Clear Points"]
                   [:div {:style {:margin-top "8px"}}
-                   @inputs]]
+                   @input-components]]
                  [:div.col-sm-9
+                  [:div]
                   [:h3 {:style {:color "red"}} "In flight"]
                   [:svg {:height "2500" :width "2500"}
                    [:polygon {:points (stringify-points @polygon)
